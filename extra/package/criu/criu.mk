522eb447641dae1e103c0513c53c838fbfa72038
A.I <ailis@paw.zone>
2015-12-22 20:00:14 +0900

    Use HOST_CFLAGS

--- extra/package/criu/criu.mk
+++ extra/package/criu/criu.mk
@@ -13,7 +13,7 @@ CRIU_LICENSE_FILES = COPYING
 define CRIU_BUILD_CMDS
 	$(RM) $(@D)/protobuf/google/protobuf/descriptor.proto
 	cp $(HOST_DIR)/usr/include/google/protobuf/descriptor.proto $(@D)/protobuf/google/protobuf/
-	$(TARGET_MAKE_ENV) $(MAKE1) CC="$(HOSTCC) -idirafter $(HOST_DIR)/usr/include $(HOST_LDFLAGS)" \
+	$(TARGET_MAKE_ENV) $(MAKE1) CC="$(HOSTCC) $(HOST_CFLAGS) $(HOST_LDFLAGS)" \
 		AR="$(HOSTAR)" -C $(@D) criu
 endef
 
