6ae473ac7031b65f6adf16d7ec91b317a674d510
A.I <ailis@paw.zone>
2016-03-08 15:25:32 +0900

    Upgrade criu to v2.0

--- extra/package/criu/criu.mk
+++ extra/package/criu/criu.mk
@@ -4,21 +4,23 @@
 #
 ################################################################################
 
-CRIU_VERSION = v1.7.2
+CRIU_VERSION = v2.0
 CRIU_SITE = $(call github,xemul,criu,$(CRIU_VERSION))
-CRIU_DEPENDENCIES = host-protobuf-c host-protobuf protobuf-c tar iproute2
+CRIU_DEPENDENCIES = host-libcap protobuf-c libnl iproute2 tar
 CRIU_LICENSE = GPLv2 (programs), LGPLv2.1 (libraries)
 CRIU_LICENSE_FILES = COPYING
 
+CRIU_CFLAGS = $(HOST_CFLAGS) --sysroot=$(STAGING_DIR) -I$(STAGING_DIR)/usr/include/libnl3
+CRIU_MAKE_OPTS = PREFIX=/usr CC="$(HOSTCC) $(CRIU_CFLAGS) $(HOST_LDFLAGS)" AR="$(HOSTAR)"
+
 define CRIU_BUILD_CMDS
-	$(RM) $(@D)/protobuf/google/protobuf/descriptor.proto
-	cp $(HOST_DIR)/usr/include/google/protobuf/descriptor.proto $(@D)/protobuf/google/protobuf/
-	$(TARGET_MAKE_ENV) $(MAKE1) CC="$(HOSTCC) $(HOST_CFLAGS) $(HOST_LDFLAGS)" \
-		AR="$(HOSTAR)" -C $(@D) criu
+	$(RM) $(@D)/images/google/protobuf/descriptor.proto
+	cp $(HOST_DIR)/usr/include/google/protobuf/descriptor.proto $(@D)/images/google/protobuf/
+	$(TARGET_MAKE_ENV) $(MAKE1) $(CRIU_MAKE_OPTS) -C $(@D) criu
 endef
 
 define CRIU_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/criu $(TARGET_DIR)/usr/sbin
+	$(TARGET_MAKE_ENV) $(MAKE1) $(CRIU_MAKE_OPTS) -C $(@D) DESTDIR=$(TARGET_DIR) install-criu
 endef
 
 $(eval $(generic-package))
