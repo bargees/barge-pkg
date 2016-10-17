f5d4da84ebe29b52c0ea66c029e364776989e216
A.I <ailis@paw.zone>
2016-10-17 09:17:34 -0700

    Upgrade criu to v2.7

--- extra/package/criu/criu.mk
+++ extra/package/criu/criu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CRIU_VERSION = v2.6
+CRIU_VERSION = v2.7
 CRIU_SITE = $(call github,xemul,criu,$(CRIU_VERSION))
 CRIU_DEPENDENCIES = host-libcap protobuf-c libnl iproute2 tar
 CRIU_LICENSE = GPLv2 (programs), LGPLv2.1 (libraries)
