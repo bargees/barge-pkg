bdc226f6bf6fa943542654660c31bcf3243f56a7
A.I <ailis@paw.zone>
2017-07-18 15:19:01 -0700

    criu: revise makefile

--- extra/package/criu/criu.mk
+++ extra/package/criu/criu.mk
@@ -6,12 +6,12 @@
 
 CRIU_VERSION = v3.3
 CRIU_SITE = $(call github,xemul,criu,$(CRIU_VERSION))
-CRIU_DEPENDENCIES = host-libcap protobuf-c libnl libnet iproute2 tar
+CRIU_DEPENDENCIES = libcap protobuf-c libnl libnet iproute2 tar
 CRIU_LICENSE = GPLv2 (programs), LGPLv2.1 (libraries)
 CRIU_LICENSE_FILES = COPYING
 
 CRIU_MAKE_ENV = $(TARGET_MAKE_ENV)
-CRIU_CFLAGS = $(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include/libnl3 -I$(HOST_DIR)/usr/include
+CRIU_CFLAGS = $(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/libnl3
 
 ifeq ($(BR2_x86_64),y)
 CRIU_HOSTCFLAGS = -DCONFIG_X86_64
@@ -21,15 +21,15 @@ endif
 ifeq ($(BR2_arm),y)
 ifeq ($(BR2_ARM_CPU_ARMV6),y)
 CRIU_CFLAGS += -march=armv6
-CRIU_HOSTCFLAGS = "-DCONFIG_ARMV6 -DNO_RELOCS"
+CRIU_HOSTCFLAGS = -DCONFIG_ARMV6 -DNO_RELOCS
 else ifeq ($(BR2_ARM_CPU_ARMV7A),y)
 CRIU_CFLAGS += -march=armv7-a
-CRIU_HOSTCFLAGS = "-DCONFIG_ARMV7 -DNO_RELOCS"
+CRIU_HOSTCFLAGS = -DCONFIG_ARMV7 -DNO_RELOCS
 endif
 endif
 
 CRIU_MAKE_OPTS = PREFIX=/usr CC="$(TARGET_CC) $(CRIU_CFLAGS) $(TARGET_LDFLAGS)" \
-	HOSTCFLAGS=$(CRIU_HOSTCFLAGS)
+	HOSTCFLAGS="$(CRIU_HOSTCFLAGS)"
 
 define CRIU_BUILD_CMDS
 	$(RM) $(@D)/images/google/protobuf/descriptor.proto
