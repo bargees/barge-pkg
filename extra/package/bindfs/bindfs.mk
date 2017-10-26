9863f94e3027e2426a2ad010efe3218e8f61255d
A.I <ailis@paw.zone>
2017-10-26 10:48:52 -0700

    Upgrade bindfs to 1.13.8

--- extra/package/bindfs/bindfs.mk
+++ extra/package/bindfs/bindfs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BINDFS_VERSION = 1.13.7
+BINDFS_VERSION = 1.13.8
 BINDFS_SOURCE = bindfs-$(BINDFS_VERSION).tar.gz
 BINDFS_SITE = http://bindfs.org/downloads
 BINDFS_DEPENDENCIES = host-pkgconf libfuse
