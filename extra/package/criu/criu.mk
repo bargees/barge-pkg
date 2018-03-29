3348f49688b621f3bbf7f49992278902d2b7db07
A.I <ailis@paw.zone>
2018-03-29 15:20:29 -0700

    Upgrade criu to v3.8.1

--- extra/package/criu/criu.mk
+++ extra/package/criu/criu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CRIU_VERSION = v3.8
+CRIU_VERSION = v3.8.1
 CRIU_SITE = $(call github,checkpoint-restore,criu,$(CRIU_VERSION))
 CRIU_DEPENDENCIES = libcap protobuf-c libnl libnet iproute2 tar
 CRIU_LICENSE = GPLv2 (programs), LGPLv2.1 (libraries)
