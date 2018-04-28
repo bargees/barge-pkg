60d319fe8fd255ad5b1f60ea21b9dffd1d2bc2ee
A.I <ailis@paw.zone>
2018-04-27 18:05:49 -0700

    Upgrade singularity to v2.5.1

--- extra/package/singularity/singularity.mk
+++ extra/package/singularity/singularity.mk
@@ -4,14 +4,18 @@
 #
 ################################################################################
 
-SINGULARITY_VERSION = 2.4.6
-SINGULARITY_SOURCE = singularity-$(SINGULARITY_VERSION).tar.gz
-SINGULARITY_SITE = https://github.com/singularityware/singularity/releases/download/$(SINGULARITY_VERSION)
+SINGULARITY_VERSION = 2.5.1
+SINGULARITY_SITE = $(call github,singularityware,singularity,$(SINGULARITY_VERSION))
 SINGULARITY_LICENSE = BSD-3-Clause
 SINGULARITY_LICENSE_FILES = LICENSE.md
 
-SINGULARITY_DEPENDENCIES = python ca-certificates squashfs getent tar libcurl file
+SINGULARITY_DEPENDENCIES = python ca-certificates squashfs getent tar libcurl file libarchive
 
 SINGULARITY_MAKE = $(MAKE1)
+
+define SINGULARITY_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+SINGULARITY_PRE_CONFIGURE_HOOKS += SINGULARITY_RUN_AUTOGEN
 
 $(eval $(autotools-package))
