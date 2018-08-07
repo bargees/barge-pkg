105863ce0aca64351261d35337aff563400c2041
A.I <ailis@paw.zone>
2018-08-07 10:27:34 -0700

    Upgrade singularity to v2.6.0

--- extra/package/singularity/singularity.mk
+++ extra/package/singularity/singularity.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SINGULARITY_VERSION = 2.5.2
+SINGULARITY_VERSION = 2.6.0
 SINGULARITY_SITE = $(call github,singularityware,singularity,$(SINGULARITY_VERSION))
 SINGULARITY_LICENSE = BSD-3-Clause
 SINGULARITY_LICENSE_FILES = LICENSE.md
